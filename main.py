da7ad711ed12a9342c1c6825cac967bb4d9257cf
SAMUEL MARQUES DA COSTA <122222762+samuel-Mdcosta@users.noreply.github.com>
2026-05-15 09:08:22 -0400

    Atualizar o main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class RequisicaoQuizz(BaseModel):
 class RequisicaoLlm(BaseModel):
     texto: str
 
-MODEL = "gemini-3-flash-preview"
+MODEL = "gemini-2.0-flash"
 GENERATE_CONFIG = types.GenerateContentConfig(temperature=0.1, max_output_tokens=2048)
 
 class Menu():
